d8db02c5b870483ef696a747ccd32ab376dc28ce
ttorti <torti.tommaso@gmail.com>
2018-09-24 16:25:46 +0200

    rename

--- plot_graph.js
+++ plot_graph.js
@@ -7,7 +7,7 @@ var chart = new Chart(ctx, {
 	data: {
 labels: [["now - 0 days"], ["now - 1 days"], ["now - 2 days"], ["now - 3 days"], ["now - 4 days"]],
     datasets: [{
-			label: "Reservations",
+			label: "Elements",
 			backgroundColor: 'lightblue',
 			borderColor: 'royalblue',
 data: ["32\n", "2150\n", "1744\n", "2359\n", "2440\n"]
